3eac0a4d5aea752fe814a6b20bb5dc7375bf07b7
Bala Luguja <bluguja@gmail.com>
2020-10-15 08:07:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do # created a controller action that responds to a GET request 
+        erb:index #loaded index.erb in browser
+    end
 
 end
